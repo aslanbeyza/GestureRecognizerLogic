863afc3224962f045e3b982602019ffd7316d0b4
aslanbeyza <aslanbeyza3413@gmail.com>
2025-02-22 21:26:03 +0300

    💕

--- GestureRecognizerApp/ViewController.swift
+++ GestureRecognizerApp/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var isfirstImage = true
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
     }
-
-
+    // bir tane jest algılayıcım olsun imageView tıklanınca  değiştireyim görseli tıklanabilir veya tıklanamaz yapabiliriz
+    @objc func changePic() {
+        // print("tapped");
+      
+        if isfirstImage == true {
+            imageView.image = UIImage(named: "2")
+            myLabel.text = "oyy yerimm yerimm"
+            isfirstImage = false
+        }else{
+            imageView.image = UIImage(named: "1")
+            myLabel.text = "Aşklarımm"
+            isfirstImage = true
+        }
+    }
 }
 
